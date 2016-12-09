f5784e3edca32e06809524a4ceb3212931c9f321
Benjamin Poile <benp51@outlook.com>
2016-12-09 10:30:35 -0800

    in the middle of something but i have to go to class :cry:

--- imgur.py
+++ imgur.py
@@ -1,12 +1,44 @@
-import urllib,os,sys
+import urllib, os, sys, validators
+from argparse import ArgumentParser
 
-#sys.argv == imgur link
+def exit(message=""):
+    print(message)
+    sys.exit()
 
-imgur_link = "http://i.imgur.com/"+sys.argv[1]+".png" #add direct image link, run script and it will download the mp3 for you. ie "1zgGEN8"
-imgur_name = imgur_link.split("/")[3]
+def valid_file(x):
+    if not os.path.exists(x): # checks if file 'x' exists, but doesn't open
+        exit("{0} does not exist".format(x))
+    return x
 
-urllib.urlretrieve(imgur_link, imgur_name)
+def strip_link(url):
+    if validators.url(url) == True:
+        return url.split("/")[3]
+    else:
+        exit("[!] Not a valid URL")
 
-command = "python pngify.py -i "+imgur_name+" -o "+imgur_name+".mp3"
+def main():
+    parser = ArgumentParser(description="Transcode text between PNGs")
+    parser.add_argument("-l", "--link", dest="link_input", help="The text or image file to transcode", metavar="STRING")
+    parser.add_argument("-o", "--output", dest="file_output", help="The file name to output the result into (whether it be an image, or other file type)", metavar="FILE")
 
-os.system(command)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+
+    args = vars(parser.parse_args())
+
+    if args['link_input'] != None:
+        output_filename = args['file_output']
+        url = args['link_input']
+        if output_filename == None:
+            print "output is none"
+            output_filename = strip_link(url)
+            urllib.urlretrieve(url, "image_temp")
+            os.system("python pngify.py -i image_temp -o "+output_filename+".mp3")
+        else:
+            print "output exists"
+            urllib.urlretrieve(url, "image_temp")
+            os.system("python pngify.py -i image_temp -o"+output_filenamet+".mp3")
+
+if __name__ == "__main__":
+    main()
